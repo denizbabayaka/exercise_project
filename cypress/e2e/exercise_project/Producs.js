359fef5dbc9522727d8e33cee3738661636ff23f
denizbabayaka@gmail.com <denizbabayaka@gmail.com>
2023-06-07 19:13:10 -0400

    second commit

--- cypress/e2e/exercise_project/Producs.js
+++ cypress/e2e/exercise_project/Producs.js
@@ -1,15 +1,17 @@
 /// <reference types="Cypress"/>
-/// <reference types="Cypress"/>
 
-describe('Add Products', () => {
-    it('Add product to basket', () => {
-         cy.visit("https://www.saucedemo.com/");
-         cy.get("#user-name").type("standard_user");
-         cy.get("#password").type("secret_sauce");
-         cy.get("#login-button").click();
-         cy.xpath("(//div[@class='inventory_item_description'])").each(($el,index,$list)=>{
-            if($el.text()=='carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.')
-         })
-        
+describe("Add Products", () => {
+  it("Add product to basket", () => {
+    cy.visit("https://www.saucedemo.com/");
+    cy.get("#user-name").type("standard_user");
+    cy.get("#password").type("secret_sauce");
+    cy.get("#login-button").click();
+    cy.get(".inventory_item_description").each(($el, index, $list) => {
+      if ($el.text().includes("carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.")){
+          
+       
+        cy.xpath("//button[@id='add-to-cart-sauce-labs-backpack']").click();
+      }
     });
+  });
 });
