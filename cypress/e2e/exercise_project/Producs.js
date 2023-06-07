2fe492e68c6a71075bf76307ad4959bc07e9a701
denizbabayaka@gmail.com <denizbabayaka@gmail.com>
2023-06-07 19:28:24 -0400

    forth commit

--- cypress/e2e/exercise_project/Producs.js
+++ cypress/e2e/exercise_project/Producs.js
@@ -1,16 +1,21 @@
 /// <reference types="Cypress"/>
 
 describe("Add Products", () => {
+
+      before(function () {
+        cy.fixture("example").then(function (data) {
+          globalThis.data = data;
+        });
+      });
   it("Add product to basket", () => {
     cy.visit("https://www.saucedemo.com/");
     cy.get("#user-name").type("standard_user");
     cy.get("#password").type("secret_sauce");
     cy.get("#login-button").click();
     cy.get(".inventory_item_description").each(($el, index, $list) => {
-      if ($el.text().includes("carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.")){
-          
-       
+      if ($el.text().includes(data.products)){
         cy.xpath("//button[@id='add-to-cart-sauce-labs-backpack']").click();
+        //button[@class='btn btn_primary btn_small btn_inventory']
       }
     });
   });
