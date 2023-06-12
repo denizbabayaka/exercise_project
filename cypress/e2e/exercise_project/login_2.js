b25486d6bfb68d79d358ab26c928de2e71fc4ee4
denizbabayaka@gmail.com <denizbabayaka@gmail.com>
2023-06-12 17:16:01 -0400

    commit

---
+++ cypress/e2e/exercise_project/login_2.js
@@ -0,0 +1,35 @@
+/// <reference types="Cypress" />
+
+describe("My Test Suite", () => {
+  beforeEach(() => {
+    cy.fixture("example_arr.json").then((data) => {
+      globalThis.data = data;
+      cy.visit("/");
+    });
+})
+
+    it("should perform tests for each data object", () => {
+      data.forEach((test) => {
+        if (test.test_name ==="negative_test") {
+          // Test logic for positive case
+          cy.get('[data-test="username"]').type(test.user_name);
+          cy.get('[data-test="password"]').type(test.password);
+          cy.get('[data-test="login-button"]').click();
+          ;
+          cy.xpath("//h3[@data-test='error']").should(
+            "have.text",
+            test.message
+          );
+          
+        } else {
+          // Test logic for negative case
+          
+          cy.get('[data-test="username"]').type(test.user_name);
+          cy.get('[data-test="password"]').type(test.password);
+          cy.get('[data-test="login-button"]').click();
+          cy.title().should("include", "Swag Labs");
+        }
+      });
+    });
+  });
+
